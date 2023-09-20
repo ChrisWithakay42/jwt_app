7f056731119b78ec8336df0f7d1c52ab7b0c84c4
ChrisWithakay42 <codewithakay@gmail.com>
2023-09-20 20:04:00 +0100

    Fix axiosPrivate import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,17 @@ import Login from "./components/Login"
 import {Routes, Route} from "react-router-dom";
 import Layout from "./components/Layout";
 import LinkPage from "./components/LinkPage";
-import Unatuthorized from "./components/Unatuthorized";
+import Unauthorized from "./components/Unauthorized";
 import RequireAuth from "./components/RequireAuth";
 import Home from "./components/Home"
+import Admin from "./components/Admin";
 
 const ROLES = {
-  'User': 2001,
-  'Editor': 1984,
-  'Admin': 5150
+    'User': 2001,
+    'Editor': 1984,
+    'Admin': 5150
 }
+
 function App() {
 
     return (
@@ -20,10 +22,14 @@ function App() {
                 <Route path={"register"} element={<Register/>}/>
                 <Route path={"login"} element={<Login/>}/>
                 <Route path={"linkpage"} element={<LinkPage/>}/>
-                <Route path={"unauthozied"} element={<Unatuthorized/>}/>
+                <Route path={"unauthozied"} element={<Unauthorized/>}/>
 
                 <Route element={<RequireAuth allowedRoles={[ROLES.User]}/>}>
-                    <Route path="/" element={<Home />}/>
+                    <Route path="/" element={<Home/>}/>
+                </Route>
+
+                <Route element={<RequireAuth allowedRoles={[ROLES.Admin]}/>}>
+                    <Route path="admin" element={<Admin />}/>
                 </Route>
             </Route>
         </Routes>
