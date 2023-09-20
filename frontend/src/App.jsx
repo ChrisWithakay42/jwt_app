0526f121a3cd5a096b302e828dbfcad3696dbf49
ChrisWithakay42 <codewithakay@gmail.com>
2023-09-20 19:01:59 +0100

    Unauthorized component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Login from "./components/Login"
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import LinkPage from "./components/LinkPage";
+import Unatuthorized from "./components/Unatuthorized";
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
               <Route path={"register"} element={<Register />}/>
               <Route path={"login"} element={<Login />}/>
               <Route path={"linkpage"} element={<LinkPage />}/>
+              <Route path={"unauthozied"} element={<Unatuthorized />}/>
           </Route>
       </Routes>
   );
