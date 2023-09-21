18b87d3106e11cf329cf74088ecfea49cddd3229
ChrisWithakay42 <codewithakay@gmail.com>
2023-09-21 17:40:42 +0100

    delete redundant file

--- backend/auth.py
+++
@@ -1,9 +0,0 @@
-from functools import wraps
-
-from flask import request
-
-
-def token_required(f: callable):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        token = request.args.get('token')
